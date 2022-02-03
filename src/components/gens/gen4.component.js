175496a2f7c8b15bec7ef97189936300e8006bd8
Abid Hasan Mahmud <41523592+abidmahmud@users.noreply.github.com>
2022-02-03 17:14:10 +0600

    card added

--- src/components/gens/gen4.component.js
+++ src/components/gens/gen4.component.js
@@ -3,66 +3,76 @@ import Gen5 from "./gen5.component";
 const Gen4 = (props) => {
     const { father_id, mother_id, males, females } = props;
     return (
-        <>
-            {/* <h1>Generation 4</h1> */}
-            {males.map((male, index) => {
-                if (
-                    male.father_id == father_id &&
-                    male.mother_id == mother_id
-                ) {
-                    return (
-                        <div key={index}>
-                            <h1>Generation 4</h1>
-                            <h1>
-                                {male.id} --- {male.name} ==={" "}
-                                {females.map((female, index) => {
-                                    if (female.id == male.spouse_id) {
-                                        return (
-                                            <span key={index}>
-                                                {" "}
-                                                ==== {female.name}
-                                            </span>
-                                        );
-                                    }
-                                })}
-                            </h1>
-                            <hr />
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        {males.map((male, index) => {
+                            if (
+                                male.father_id == father_id &&
+                                male.mother_id == mother_id
+                            ) {
+                                return (
+                                    <div key={index}>
+                                        <h1>Generation 4</h1>
+                                        <h1>
+                                            {male.id} --- {male.name} ==={" "}
+                                            {females.map((female, index) => {
+                                                if (female.id == male.spouse_id) {
+                                                    return (
+                                                        <span key={index}>
+                                                            {" "}
+                                                            ==== {female.name}
+                                                        </span>
+                                                    );
+                                                }
+                                            })}
+                                        </h1>
+                                        <hr />
 
-                            <Gen5 father_id={male.id} mother_id={male.spouse_id} males={males} females={females} />
-                        </div>
-                    );
-                }
-            })}
-            {females.map((female, index) => {
-                if (
-                    female.father_id == father_id &&
-                    female.mother_id == mother_id
-                ) {
-                    return (
-                        <div key={index}>
-                            <h1>Generation 4</h1>
-                            <h1>
-                                {female.id} --- {female.name} ==={" "}
-                                {males.map((male, index) => {
-                                    if (male.id == female.spouse_id) {
-                                        return (
-                                            <span key={index}>
-                                                {" "}
-                                                ==== {male.name}
-                                            </span>
-                                        );
-                                    }
-                                })}
-                            </h1>
-                            <hr />
+                                        <Gen5 father_id={male.id} mother_id={male.spouse_id} males={males} females={females} />
+                                    </div>
+                                );
+                            }
+                        })}
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        {females.map((female, index) => {
+                            if (
+                                female.father_id == father_id &&
+                                female.mother_id == mother_id
+                            ) {
+                                return (
+                                    <div key={index}>
+                                        <h1>Generation 4</h1>
+                                        <h1>
+                                            {female.id} --- {female.name} ==={" "}
+                                            {males.map((male, index) => {
+                                                if (male.id == female.spouse_id) {
+                                                    return (
+                                                        <span key={index}>
+                                                            {" "}
+                                                            ==== {male.name}
+                                                        </span>
+                                                    );
+                                                }
+                                            })}
+                                        </h1>
+                                        <hr />
 
-                            <Gen5 father_id={female.spouse_id} mother_id={female.id} males={males} females={females} />
-                        </div>
-                    );
-                }
-            })}
-            <hr />
-        </>
+                                        <Gen5 father_id={female.spouse_id} mother_id={female.id} males={males} females={females} />
+                                    </div>
+                                );
+                            }
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/components/gens/gen5.component.js
+++ src/components/gens/gen5.component.js
@@ -3,62 +3,73 @@
 const Gen5 = (props) => {
     const { father_id, mother_id, males, females } = props;
     return (
-        <>
-            {/* <h1>Generation 4</h1> */}
-            {males.map((male, index) => {
-                if (
-                    male.father_id == father_id &&
-                    male.mother_id == mother_id
-                ) {
-                    return (
-                        <div key={index}>
-                            <h1>Generation 5</h1>
-                            <h1>
-                                {male.id} --- {male.name} ==={" "}
-                                {females.map((female, index) => {
-                                    if (female.id == male.spouse_id) {
-                                        return (
-                                            <span key={index}>
-                                                {" "}
-                                                ==== {female.name}
-                                            </span>
-                                        );
-                                    }
-                                })}
-                            </h1>
-                            <hr />
-                        </div>
-                    );
-                }
-            })}
-            {females.map((female, index) => {
-                if (
-                    female.father_id == father_id &&
-                    female.mother_id == mother_id
-                ) {
-                    return (
-                        <div key={index}>
-                            <h1>Generation 5</h1>
-                            <h1>
-                                {female.id} --- {female.name} ==={" "}
-                                {males.map((male, index) => {
-                                    if (male.id == female.spouse_id) {
-                                        return (
-                                            <span key={index}>
-                                                {" "}
-                                                ==== {male.name}
-                                            </span>
-                                        );
-                                    }
-                                })}
-                            </h1>
-                            <hr />
-                        </div>
-                    );
-                }
-            })}
-            <hr />
-        </>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        {males.map((male, index) => {
+                            if (
+                                male.father_id == father_id &&
+                                male.mother_id == mother_id
+                            ) {
+                                return (
+                                    <div key={index}>
+                                        <h1>Generation 5</h1>
+                                        <h1>
+                                            {male.id} --- {male.name} ==={" "}
+                                            {females.map((female, index) => {
+                                                if (female.id == male.spouse_id) {
+                                                    return (
+                                                        <span key={index}>
+                                                            {" "}
+                                                            ==== {female.name}
+                                                        </span>
+                                                    );
+                                                }
+                                            })}
+                                        </h1>
+                                        <hr />
+                                    </div>
+                                );
+                            }
+                        })}
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        {females.map((female, index) => {
+                            if (
+                                female.father_id == father_id &&
+                                female.mother_id == mother_id
+                            ) {
+                                return (
+                                    <div key={index}>
+                                        <h1>Generation 5</h1>
+                                        <h1>
+                                            {female.id} --- {female.name} ==={" "}
+                                            {males.map((male, index) => {
+                                                if (male.id == female.spouse_id) {
+                                                    return (
+                                                        <span key={index}>
+                                                            {" "}
+                                                            ==== {male.name}
+                                                        </span>
+                                                    );
+                                                }
+                                            })}
+                                        </h1>
+                                        <hr />
+                                    </div>
+                                );
+                            }
+                        })}
+                        <hr />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
