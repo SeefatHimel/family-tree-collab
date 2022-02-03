6513d161974db6658667d747fb7cb5f9eb583357
Seefat Hossain Himel <seefathimel1@gmail.com>
2022-02-03 17:47:47 +0600

    Fixed all :)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ function App() {
   return (
     <>
       <h1>App =====</h1>
+      <div style={{width:"250%" , textAlign:'center'}}>
+
       <Gen />
+      </div>
     </>
   );
 }
--- src/components/gens/gen4.component.js
+++ src/components/gens/gen4.component.js
@@ -3,26 +3,28 @@ import Gen5 from "./gen5.component";
 const Gen4 = (props) => {
     const { father_id, mother_id, males, females } = props;
     return (
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        {males.map((male, index) => {
-                            if (
-                                male.father_id == father_id &&
-                                male.mother_id == mother_id
-                            ) {
-                                return (
+        <>
+            <div className="row">
+                {males.map((male, index) => {
+                    if (
+                        male.father_id == father_id &&
+                        male.mother_id == mother_id
+                    ) {
+                        return (
+                            <div className="col">
+                                <div className="card">
                                     <div key={index}>
                                         <h1>Generation 4</h1>
                                         <h1>
-                                            {male.id} --- {male.name} ==={" "}
+                                            {male.id} --- {male.name} ===
                                             {females.map((female, index) => {
-                                                if (female.id == male.spouse_id) {
+                                                if (
+                                                    female.id == male.spouse_id
+                                                ) {
                                                     return (
                                                         <span key={index}>
-                                                            {" "}
-                                                            ==== {female.name}
+                                                            ====
+                                                            {female.name}
                                                         </span>
                                                     );
                                                 }
@@ -30,32 +32,37 @@ const Gen4 = (props) => {
                                         </h1>
                                         <hr />
 
-                                        <Gen5 father_id={male.id} mother_id={male.spouse_id} males={males} females={females} />
+                                        <Gen5
+                                            father_id={male.id}
+                                            mother_id={male.spouse_id}
+                                            males={males}
+                                            females={females}
+                                        />
                                     </div>
-                                );
-                            }
-                        })}
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        {females.map((female, index) => {
-                            if (
-                                female.father_id == father_id &&
-                                female.mother_id == mother_id
-                            ) {
-                                return (
+                                </div>
+                            </div>
+                        );
+                    }
+                })}
+                {females.map((female, index) => {
+                    if (
+                        female.father_id == father_id &&
+                        female.mother_id == mother_id
+                    ) {
+                        return (
+                            <div className="col">
+                                <div className="card">
                                     <div key={index}>
                                         <h1>Generation 4</h1>
                                         <h1>
-                                            {female.id} --- {female.name} ==={" "}
+                                            {female.id} --- {female.name}
+                                            ===
                                             {males.map((male, index) => {
-                                                if (male.id == female.spouse_id) {
+                                                if (
+                                                    male.id == female.spouse_id
+                                                ) {
                                                     return (
                                                         <span key={index}>
-                                                            {" "}
                                                             ==== {male.name}
                                                         </span>
                                                     );
@@ -64,15 +71,20 @@ const Gen4 = (props) => {
                                         </h1>
                                         <hr />
 
-                                        <Gen5 father_id={female.spouse_id} mother_id={female.id} males={males} females={females} />
+                                        <Gen5
+                                            father_id={female.spouse_id}
+                                            mother_id={female.id}
+                                            males={males}
+                                            females={females}
+                                        />
                                     </div>
-                                );
-                            }
-                        })}
-                    </div>
-                </div>
+                                </div>
+                            </div>
+                        );
+                    }
+                })}
             </div>
-        </div>
+        </>
     );
 };
 
